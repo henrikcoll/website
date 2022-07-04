41ab4dc6c753c98813c8ee71901e41c1b4e78fc5
Henrik Næss <henrik@heka.no>
2022-07-04 18:25:09 +0200

    Changed contact email

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,7 +2,7 @@
 	<div class="py-32 text-center" id="contact">
 		<h2 class="mb-6 text-4xl font-semibold text-black dark:text-white">Lyst til å jobbe sammen?</h2>
 		<p class="text-slate-900 text-lg dark:text-white">
-			<a href="mailto:henrik@heka.no" class="border-b hover:border-b-2 border-b-gray-400 font-medium text-sky-400 hover:border-b-sky-600">henrik@heka.no</a>
+			<a href="mailto:kontakt@heka.no" class="border-b hover:border-b-2 border-b-gray-400 font-medium text-sky-400 hover:border-b-sky-600">kontakt@heka.no</a>
 		</p>
 	</div>
 </template>
